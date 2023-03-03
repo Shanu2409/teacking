9dc7849b38595220297cda92f54d793b46d92dea
shanu <shanusingh2409@gmail.com>
2023-03-03 19:41:53 +0530

    testing

--- Teackinggg/Models/Issue.cs
+++ Teackinggg/Models/Issue.cs
@@ -3,8 +3,8 @@
     public class Issue
     {
         public uint Id { get; set; }
-        public string Title { get; set; }
-        public string Descriptioni { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Descriptioni { get; set; } = string.Empty;
         public IssueType Isu { get; set; }
         public Priority Priority { get; set; }
         public DateTime Created { get; set; }
