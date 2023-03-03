45a131001a594c4fcaadabf6139f008b1325bf23
shanu <shanusingh2409@gmail.com>
2023-03-03 19:38:09 +0530

    first

---
+++ Teackinggg/Models/Issue.cs
@@ -0,0 +1,27 @@
+﻿namespace Teackinggg.Models
+{
+    public class Issue
+    {
+        public uint Id { get; set; }
+        public string Title { get; set; }
+        public string Descriptioni { get; set; }
+        public IssueType Isu { get; set; }
+        public Priority Priority { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime? Completed { get; set; }
+    }
+
+    public enum Priority
+    {
+        Low,
+        Medium,
+        High
+    }
+
+    public enum IssueType
+    {
+        Feature,
+        Bug,
+        Documentation
+    }
+}
